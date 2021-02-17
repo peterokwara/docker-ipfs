4f5cfcc6bc9d0c0db088711d955807113805a5f7
Peter Okwara <ojakaapeter@gmail.com>
2021-02-17 20:29:48 +0300

    Add start script to make things easier

---
+++ private-ipfs.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+command="$1"
+
+# Start ipfs bootstrap node
+startBootstrap () {
+
+    #swarm key
+    swarmKey
+
+    # Run the bootstrap ipfs node
+    docker-compose --log-level ERROR up -d bootstrap_node
+
+}
+
+# Start ipfs node
+startIpfs () {
+
+    # Run an ipfs node
+    docker-compose --log-level ERROR up -d ipfs_node
+
+}
+
+# Generate a swarm key
+swarmKey () {
+
+    # Generate a swarm key and output into a file 
+    docker run --rm golang:1.9 sh -c 'go get github.com/Kubuxu/go-ipfs-swarm-key-gen/ipfs-swarm-key-gen && ipfs-swarm-key-gen' >> private-network/.ipfs/swarm.key
+}
+
+# Stop all containers
+stopContainers () {
+
+  echo "Stopping containers..."
+	docker-compose --log-level ERROR down -v --remove-orphans
+}
+
+# Show help
+help () {
+  echo "usage: private-ipfs.sh [startBoostrap|startIpfs]"
+}
+
+
+# Start, stop, remove
+case "${command}" in
+        "help")
+    help
+    ;;
+        "start-bootstrap")
+    startBootstrap
+    ;;
+        "start-ipfs")
+    startIpfs
+    ;;
+         "stop")
+    stopContainers
+    ;;
+  *)
+    echo "Command not Found."
+    help
+    exit 127;
+    ;;
+esac
